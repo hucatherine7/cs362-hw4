15ee82aab345deca79f9a94899985938ae026a06
hucatherine7 <hucatherine7@gmail.com>
2021-02-06 22:45:06 -0800

    Changing test_question1 comments

--- test_question1.py
+++ test_question1.py
@@ -19,21 +19,3 @@ class Question1(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
-
-#
-# import unittest
-# from cubeVolume import cubeVolume
-#
-# class TestCubeVolume(unittest.TestCase):
-#
-#     def test_cubeVolume(self):
-#         self.assertEqual(cubeVolume(3), 27)
-#
-#     def test_negativeCubeVolume(self):
-#         self.assertEqual(cubeVolume(-1), -1)
-#
-#     def test_nonIntegerCubeVolume(self):
-#         self.assertEqual(cubeVolume("string"), -1)
-#
-# if __name__ == "__main__":
-#     unittest.main()
